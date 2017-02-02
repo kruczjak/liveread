18dea97b79db34d5e0b2424d6453585f99d82fec
kruczjak <kruczjak@gmail.com>
2017-02-02 22:38:54 +0100

    Webpack compile is done automatically

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,3 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-namespace :deploy do
-  after :compile_assets, 'rails webpacker:compile'
-end
